933406a0fbf45bf91778651e7de7d073020d95d3
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2023-10-23 00:41:16 +0200

    -hide routes can show by click on acitionIcon -hide route can make visible by click on icon on routeCard

--- src/hooks/addRouteHook.js
+++ src/hooks/addRouteHook.js
@@ -1,17 +1,23 @@
 import { useState } from "react";
+import { useBetween } from "use-between";
+import useDatabases from "./databaseHook";
 
 function useAddRoute() {
-  
   const [selectedRoute, setSelectedRoute] = useState();
   const [selectedCard, setSelectedCard] = useState();
   const [chipValue, setChipValue] = useState("startAddName");
+  const [showHideRoutes, setShowHideRoutes] = useState(false);
+
 
   return {
     selectedRoute,
     setSelectedRoute,
     selectedCard,
     setSelectedCard,
-    chipValue, setChipValue
+    chipValue,
+    setChipValue,
+    sohwHideRoutes: showHideRoutes,
+    setSohwHideRoutes: setShowHideRoutes,
   };
 }
 
