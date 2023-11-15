02935de59e64c87afa73e846c3fa10209b6064dc
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2023-11-15 03:05:44 +0100

    -add cs styles to center the DateView

--- Components/DateView/DateView.js
+++ Components/DateView/DateView.js
@@ -6,7 +6,7 @@ import CurrentDayRoutes from './CurrentDayRoutes/CurrentDayRoutes';
 import Grid from '../CustomComponents/Grid/Grid';
 
 const styles = {
-  root: {height: 500, width: 800},
+  root: {alignSelf: 'center'},
   gridStyle: {
     width: 800,
     marginHorizontal: 'auto',
@@ -14,8 +14,9 @@ const styles = {
   row: {
     flexDirection: 'row',
   },
-  col1: {flex: 4},
-  col2: {flex: 1},
+  col1: {flex: 1.4, marginHorizontal: 'auto',},
+  col2: {flex: 0.4, marginHorizontal: 'auto',},
+  col3: {flex: 1.,  marginHorizontal: 'auto',},
 };
 
 export default function DateView() {
@@ -25,12 +26,12 @@ export default function DateView() {
       cols: [
         {style: styles.col1, item: <CalenderView />},
         {style: styles.col2, item: <CurrentDayDetailsView />},
-        {style: styles.col2, item: <CurrentDayRoutes />},
+        {style: styles.col3, item: <CurrentDayRoutes />},
       ],
     },
   ];
   return (
-    <View>
+    <View style={styles.root}>
       <Grid gridStyle={styles.gridStyle} rowsAndCols={rowsAndCols} />
     </View>
   );
