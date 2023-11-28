d21fd992fd702d002f3fd69a8ac4fe0ca09ea5b6
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2023-11-28 01:42:03 +0100

    text input for a specific search and needles logic

--- src/Components/NewRoute/NewRoute.js
+++ src/Components/NewRoute/NewRoute.js
@@ -1,5 +1,11 @@
-import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, Text, TouchableOpacity, Button} from 'react-native';
+import React from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
 import {Popup} from 'react-native-windows';
 import ScrollArea from '../CustomComponents/ScrollArea/ScrollArea';
 import ButtonIcon from '../CustomComponents/ButtonSvgIcon/ButtonIcon';
@@ -10,9 +16,6 @@ import useNewRoute from '../../stores/newRouteStore';
 import NewAddressModal from './NewAddressModal/NewAddressModal';
 import useNewAddressModal from '../../stores/newAddresModalStore';
 import {useBetween} from 'use-between';
-import useDatabase from '../../stores/databaseStore';
-import AddressCard from './AddressCard/AddressCard';
-import {sortByAlphabetAscending} from '../../asserts/sortHelper';
 
 const styles = StyleSheet.create({
   root: {alignSelf: 'center', height: 800, width: 300},
@@ -44,35 +47,27 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightgray',
   },
 
+  searchTextInput: {marginBottom: 8},
+
   text: {textAlign: 'center', paddingLeft: 8},
   headline: {fontSize: 18, fontWeight: 'bold', marginTop: 8, marginBottom: 16},
 });
 export default function NewRoute() {
   const useShareNewRoute = () => useBetween(useNewRoute);
-  const {handelOnClickBackBtn, handelOnClickNewAddressBtn} = useShareNewRoute();
+  const {
+    sortValue,
+    handelOnClickBackBtn,
+    searchValue,
+    setSearchValue,
+    cards,
+    handelOnClickNewAddressBtn,
+    handelOnClickNamePill,
+    handelOnClickStreetPill,
+    handelOnClickPlzPill,
+    handelOnClickPlacePill,
+  } = useShareNewRoute();
   const useShareNewAddressModal = () => useBetween(useNewAddressModal);
   const {modalVisible} = useShareNewAddressModal();
-  const useShareDatabase = () => useBetween(useDatabase);
-  const {addresses} = useShareDatabase();
-
-  const [cards, setCards] = useState([]);
-  const handelOnClickNamePill = () => {
-    setSortValue('name');
-  };
-
-  const handelOnClickStreetPill = () => {
-    setSortValue('street');
-  };
-
-  const handelOnClickPlzPill = () => {
-    setSortValue('plz');
-  };
-
-  const handelOnClickPlacePill = () => {
-    setSortValue('place');
-  };
-
-  const [sortValue, setSortValue] = useState('name');
 
   const rowsAndCols1 = [
     {
@@ -172,62 +167,16 @@ export default function NewRoute() {
     },
   ];
 
-  useEffect(() => {
-    console.log(addresses);
-
-    switch (sortValue) {
-      case 'name': {
-        addresses.sort((a, b) => sortByAlphabetAscending(a.name, b.name));
-        // list = list.filter(route =>
-        //   getRouteFullAddressesByRouteId(route.route_id)[0].name.startsWith(
-        //     searchValue,
-        //   ),
-        // );
-
-        break;
-      }
-      case 'street': {
-        addresses.sort((a, b) => sortByAlphabetAscending(a.street, b.street));
-
-        // list = list.filter(route =>
-        //   getRouteFullAddressesByRouteId(route.route_id)[1].name.startsWith(
-        //     searchValue,
-        //   ),
-        // );
-
-        break;
-      }
-      case 'plz': {
-        addresses.sort((a, b) => sortByAlphabetAscending(a.plz, b.plz));
-        // list = list.filter(route =>
-        //   getRouteFullAddressesByRouteId(route.route_id)[0].street.startsWith(
-        //     searchValue,
-        //   ),
-        // );
-        break;
-      }
-      case 'place': {
-        addresses.sort((a, b) => sortByAlphabetAscending(a.place, b.place));
-        // list = list.filter(route =>
-        //   getRouteFullAddressesByRouteId(route.route_id)[1].street.startsWith(
-        //     searchValue,
-        //   ),
-        // );
-        break;
-      }
-      default: {
-      }
-    }
-    const cardsList = addresses.map(address => (
-      <AddressCard key={('add', address.add_id)} address={address} />
-    ));
-    setCards(cardsList);
-  }, [addresses, sortValue]);
-
   return (
     <View style={styles.root}>
       <Text style={styles.headline}>Neue Strecke</Text>
       <Grid rowsAndCols={rowsAndCols1} style={styles.gridStyle} />
+      <TextInput
+        placeholder="Suche"
+        value={searchValue}
+        onChangeText={setSearchValue}
+        style={styles.searchTextInput}
+      />
       <ScrollArea itemsList={cards} />
       <Grid rowsAndCols={rowsAndCols2} style={styles.gridStyle} />
       <Popup isOpen={modalVisible}>
