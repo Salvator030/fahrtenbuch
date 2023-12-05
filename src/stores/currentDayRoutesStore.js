2903692bfe9432e08642c094b17c58de2ae0a167
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2023-12-06 00:46:16 +0100

    drivenRoutesCards sort by Logical Order work not like the yellow of the ege

--- src/stores/currentDayRoutesStore.js
+++ src/stores/currentDayRoutesStore.js
@@ -4,10 +4,12 @@ import useDatabase from '../stores/databaseStore';
 import DrivenRoutesCards from '../Components/DateView/CurrentDayRoutes/DrivenRoutesCards/DrivenRoutesCards';
 import {View, Text} from 'react-native';
 import useCalender from './calenderStore';
+import {sortDrivenRouteByLogicalOrder} from '../asserts/sortHelper';
 
 export default function useCurrentDayRoutes() {
   const useShareDatabase = () => useBetween(useDatabase);
-  const {drivenRoutes} = useShareDatabase();
+  const {drivenRoutes, getFullAddressById, getFullRouteById} =
+    useShareDatabase();
   const useShareCalendar = () => useBetween(useCalender);
   const {selectedDate} = useShareCalendar();
 
@@ -21,13 +23,25 @@ export default function useCurrentDayRoutes() {
         route.date ===
         `${selectedDate.getDate()}.${selectedDate.getMonth()}.${selectedDate.getFullYear()}`,
     );
+    list.sort((a, b) => {
+      const routeA = [
+        getFullAddressById(getFullRouteById(a.route_id).startAdd_id),
+        getFullAddressById(getFullRouteById(a.route_id).destAdd_id),
+      ];
+      const routeB = [
+        getFullAddressById(getFullRouteById(b.route_id).startAdd_id),
+        getFullAddressById(getFullRouteById(b.route_id).destAdd_id),
+      ];
+      console.log(routeA);
+      return sortDrivenRouteByLogicalOrder(routeA, routeB);
+    });
     let cards = list.map(route => (
       <DrivenRoutesCards key={route.dRoute_id} drivenRoute={route} />
     ));
 
     console.log('cards', cards.length);
     setDrivenRoutesCards(cards);
-  }, [drivenRoutes, selectedDate]);
+  }, [drivenRoutes, getFullAddressById, getFullRouteById, selectedDate]);
 
   console.log('d', drivenRoutesCards.length);
   return {drivenRoutesCards};
