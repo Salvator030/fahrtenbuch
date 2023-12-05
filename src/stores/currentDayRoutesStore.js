db13e104a7983fff35b2e559fcff9206c742c41a
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2023-12-05 23:58:10 +0100

    only show the drivenRoutes at the selected Date

--- src/stores/currentDayRoutesStore.js
+++ src/stores/currentDayRoutesStore.js
@@ -3,22 +3,31 @@ import {useBetween} from 'use-between';
 import useDatabase from '../stores/databaseStore';
 import DrivenRoutesCards from '../Components/DateView/CurrentDayRoutes/DrivenRoutesCards/DrivenRoutesCards';
 import {View, Text} from 'react-native';
+import useCalender from './calenderStore';
 
 export default function useCurrentDayRoutes() {
   const useShareDatabase = () => useBetween(useDatabase);
   const {drivenRoutes} = useShareDatabase();
+  const useShareCalendar = () => useBetween(useCalender);
+  const {selectedDate} = useShareCalendar();
 
   const [drivenRoutesCards, setDrivenRoutesCards] = useState([]);
 
   useEffect(() => {
-    console.log('useCurrentDayRoutes', drivenRoutes);
-    let cards = drivenRoutes.map(route => (
+    console.log('selectedDate', selectedDate);
+    let list = drivenRoutes;
+    list = list.filter(
+      route =>
+        route.date ===
+        `${selectedDate.getDate()}.${selectedDate.getMonth()}.${selectedDate.getFullYear()}`,
+    );
+    let cards = list.map(route => (
       <DrivenRoutesCards key={route.dRoute_id} drivenRoute={route} />
     ));
 
     console.log('cards', cards.length);
     setDrivenRoutesCards(cards);
-  }, [drivenRoutes]);
+  }, [drivenRoutes, selectedDate]);
 
   console.log('d', drivenRoutesCards.length);
   return {drivenRoutesCards};
